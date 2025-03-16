b43711a9d23c53c3ca38ff9c898c43d54b9647d9
bikramjitchawla <bkjit99@gmail.com>
2025-03-16 22:28:34 +0100

    calico setup for local setup

--- start.sh
+++ start.sh
@@ -7,6 +7,16 @@ kind create cluster --config=Kind/cluster.yaml || true
 echo "Cluster created successfully."
 
 echo "Deploying manifests with Skaffold..."
+
+kubectl create namespace argocd || true
+echo "ArgoCD namespace created successfully."
+
 cd Kind; skaffold run; cd ..
+cd calico; skaffold run --filename skaffold-operator.yaml; cd ..
+ca calico; skaffold run --filename skaffold-resource.yaml; cd ..
 
 echo "Skaffold deployment completed."
+
+
+
+
