ad2452a202cdf1d41a3d20621b67e764e27a227e
bikramjitchawla <bkjit99@gmail.com>
2025-03-17 21:49:56 +0100

    calico fixed

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-set -euo pipefail
-
 echo "Creating Kind cluster..."
 kind create cluster --config=Kind/cluster.yaml || true
 
@@ -11,12 +8,8 @@ echo "Deploying manifests with Skaffold..."
 kubectl create namespace argocd || true
 echo "ArgoCD namespace created successfully."
 
-cd Kind; skaffold run; cd ..
-cd calico; skaffold run --filename skaffold-operator.yaml; cd ..
-ca calico; skaffold run --filename skaffold-resource.yaml; cd ..
+cd Kind; skaffold run; cd ..;
+cd calico; skaffold run --filename skaffold-operator.yaml; cd ..;
+cd calico; skaffold run --filename skaffold-resource.yaml; cd ..;
 
-echo "Skaffold deployment completed."
-
-
-
-
+echo "Skaffold deployment completed.";
