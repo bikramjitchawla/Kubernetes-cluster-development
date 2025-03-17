3362cb985fb02c46083cce4cb83d136ac44286f0
bikramjitchawla <bkjit99@gmail.com>
2025-03-17 22:17:12 +0100

    updated bash script

--- start.sh
+++ start.sh
@@ -6,7 +6,6 @@ echo "Cluster created successfully."
 echo "Deploying manifests with Skaffold..."
 
 kubectl create namespace argocd || true
-echo "ArgoCD namespace created successfully."
 
 cd Kind; skaffold run; cd ..;
 cd calico; skaffold run --filename skaffold-operator.yaml; cd ..;
